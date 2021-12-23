1ad9b4f4c19062a63cd597ddfd19e208e6c66157
Lhoussaine Ahammam <lahammam@e1r4p2.1337.ma>
2021-12-23 01:05:12 +0100

    fix

--- client.c
+++ client.c
@@ -1,65 +1,75 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   client.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lahammam <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/12/21 12:53:42 by lahammam          #+#    #+#             */
+/*   Updated: 2021/12/21 12:53:45 by lahammam         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
+#include<signal.h>
+#include <unistd.h>
+#include "printf/ft_printf.h"
 
-
-
-#include "mnitalk.h"
-
-
-char *char_to_bin(char c)
+char	*char_to_byte(char c)
 {
-	int i;
-	char *result;
-	int b;
+	int		i;
+	char	*result;
+	int		b;
 
 	i = 7;
-	result = ft_calloc(sizeof(char), 9);
-	while (i >= 0 )
+	result = ft_calloc(9, sizeof(char));
+	while (i >= 0)
 	{
-		b = c & (1<<i);
+		b = c & (1 << i);
 		if (b > 0)
 			result[7 - i] = '1';
 		else
-			result[7 -i] = '0';
+			result[7 - i] = '0';
 		i--;
 	}
-   return (result);
+	return (result);
 }
 
-void ft_send_char(int iPid,char *bin)
+void	ft_send_char(int iPid, char *byte)
 {
-	int i;
+	int		i;
 
 	i = 0;
-	while (bin[i])
+	while (byte[i])
 	{
-		usleep(1000);
-		if (bin[i] == '0')
+		if (byte[i] == '0')
 			kill(iPid, SIGUSR1);
 		else
 			kill(iPid, SIGUSR2);
 		i++;
+		usleep(200);
 	}
 }
 
-int main(int argc, char *argv[])
+int	main(int argc, char *argv[])
 {
-	char *bin;
-	int i;
-	int iPid;
+	char	*byte;
+	int		i;
+	int		ipid;
 
-	
 	i = 0;
-	bin = 0;
-	iPid = ft_atoi(argv[1]);
-	while(1)
+	byte = 0;
+	ipid = ft_atoi(argv[1]);
+	if (argc >= 3)
 	{
-		bin = char_to_bin(argv[2][i]);
-		ft_send_char( iPid, bin );
-		free(bin);
-		if (argv[2][i] == 0)
-			break;
-		i++;
+		while (1)
+		{
+			byte = char_to_byte(argv[2][i]);
+			ft_send_char(ipid, byte);
+			free(byte);
+			byte = 0;
+			if (argv[2][i] == 0)
+				break ;
+			i++;
+		}
 	}
-
-	
 }
